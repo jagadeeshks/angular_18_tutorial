033e045f3d5bc659fd75c008d4922696040d5790
jagadeeshazure1985@gmail.com <jagadeeshazure1985@gmail.com>
2025-05-16 11:01:58 +0530

    added client service

--- src/app/Components/client/client.component.ts
+++ src/app/Components/client/client.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Client } from '../../model/class/Client';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  standalone:true,
+  imports: [FormsModule],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css',
 })
 export class ClientComponent {
+  clientObj: Client = new Client();
+  clientList: Client[]=[];
+
+onSaveClient()
+{
+  
+}
 
 }
---
+++ src/app/services/client.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../model/class/Client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http:HttpClient) { 
+   
+  }
+
+  //  getAllCients():Observable<Client>
+  //   {
+  //     //return this.http.get("")
+  //   }
+}
